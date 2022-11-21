20ae2bc8c9f8bd45fa09cc1fdf6e49f5360f5514
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-21 18:30:43 -0300

    Configuring DataContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,8 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using RochaBank.Models;
 
 namespace RochaBank.DataContext
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<ClientType> ClientTypes { get; set; }
+        public DbSet<Company> Companys { get; set; }
+        public DbSet<Taxpayer> Taxpayers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlServer("Server=localhost,1433;Database=RochaBank;User ID=sa;Password=1q2w3e4r@#$");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
     }
 }
