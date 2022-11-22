c9eff26e5b5192b86fa768feb08be5a09571b5a8
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-22 11:50:42 -0300

    Mapping Company table

--- Data/Mapping/CompanyMap.cs
+++ Data/Mapping/CompanyMap.cs
@@ -8,6 +8,36 @@ namespace RochaBank.Data.Mapping
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Company> builder)
         {
             builder.ToTable("Company");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Cellphone)
+                .IsRequired()
+                .HasColumnName("Cellphone")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(12);
+
+            builder.Property(x => x.Cellphone)
+                .IsRequired()
+                .HasColumnName("CNPJ")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(14);
+
+            builder.Property(x => x.BankBalance)
+                .IsRequired()
+                .HasColumnName("Bank Balance")
+                .HasColumnType("DECIMAL");
+
         }
     }
 }
