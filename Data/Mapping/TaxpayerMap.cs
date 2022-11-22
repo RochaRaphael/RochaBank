3b1cf12e317cd6095148de865d29f98e9c286b06
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-22 11:53:38 -0300

    Mapping Taxpayer table

--- Data/Mapping/TaxpayerMap.cs
+++ Data/Mapping/TaxpayerMap.cs
@@ -8,6 +8,35 @@ namespace RochaBank.Data.Models
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Taxpayer> builder)
         {
             builder.ToTable("Taxpayer");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Cellphone)
+                .IsRequired()
+                .HasColumnName("Cellphone")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(12);
+
+            builder.Property(x => x.Cellphone)
+                .IsRequired()
+                .HasColumnName("CPF")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(14);
+
+            builder.Property(x => x.BankBalance)
+                .IsRequired()
+                .HasColumnName("Bank Balance")
+                .HasColumnType("DECIMAL");
         }
     }
 }
