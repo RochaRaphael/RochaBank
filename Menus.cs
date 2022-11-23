f6c02f566514e54bb85ea4e5c5c52aee2cc20c4c
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-23 14:59:29 -0300

    Manager menu added

--- Menus.cs
+++ Menus.cs
@@ -21,7 +21,18 @@ namespace RochaBank
             Console.WriteLine("3- Deposit");
             Console.WriteLine("4- Exit");
         }
+
+        public static void ManagerMenu()
+        {
+            Console.WriteLine("YOU ARE WELCOME!!");
+            Console.WriteLine("CHOSE AN OPTION - PUT A NUMBER");
+            Console.WriteLine();
+            Console.WriteLine("1- Register new client");
+            Console.WriteLine("2- Register new client type");
+            Console.WriteLine("3- Delete client");
+            Console.WriteLine("3- Delete client type");
+            Console.WriteLine("4- Exit");
+        }
     }
 
-}
 }
