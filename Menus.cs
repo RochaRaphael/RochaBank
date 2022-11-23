3e34b5cd00201f15c41aebb9699b5ceb78bf4330
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-23 14:52:11 -0300

    Client Menu added

--- Menus.cs
+++ Menus.cs
@@ -11,5 +11,17 @@ namespace RochaBank
             Console.WriteLine("3- Exit");
         }
 
+        public static void ClientMenu()
+        {
+            Console.WriteLine("YOU ARE WELCOME!!");
+            Console.WriteLine("CHOSE AN OPTION - PUT A NUMBER");
+            Console.WriteLine();
+            Console.WriteLine("1- See bank balance");
+            Console.WriteLine("2- Withdraw money");
+            Console.WriteLine("3- Deposit");
+            Console.WriteLine("4- Exit");
+        }
     }
+
+}
 }
