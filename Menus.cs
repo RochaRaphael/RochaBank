4180c04a23f4ed05672cb66bfa3df939fd44fb45
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-23 14:40:36 -0300

    Created Menus class and main menu added

---
+++ Menus.cs
@@ -0,0 +1,15 @@
+namespace RochaBank
+{
+    public static class Menus
+    {
+        public static void MainMenu()
+        {
+            Console.WriteLine("CHOSE AN OPTION - PUT A NUMBER");
+            Console.WriteLine();
+            Console.WriteLine("1- I am client");
+            Console.WriteLine("2- I am bank manager");
+            Console.WriteLine("3- Exit");
+        }
+
+    }
+}
